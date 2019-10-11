36119c2a3b636141d693af309e94836943d941b8
Nivaaz <nksehmbhi@gmail.com>
2019-10-11 16:57:05 +1100

    works.

--- dsp_task2.m
+++ dsp_task2.m
@@ -3,31 +3,27 @@
 %  sample rate Fs in Hertz, number of bits NBITS, and number of channels NCHANS. 
 clc
 clear
+close all
 %highest frequency shoulld be at 200Hz
 fs = 8e3;
-nbits = 1024;
+nbits = 8;
 nchans = 1;
 L = 8; % decimation ratio
 M = 8; % upsampling ratio
+format short
 ar = audiorecorder(fs, nbits, nchans); %sample the data
-pause(1);  %wait for a seocnd.
-pause(ar); %stop the player recording.
-data = getaudiodata(ar, 'int16');
-% p = play(ar); %play the recording.
+recordblocking(ar, 1);  %wait for a seocnd.
+
+data = getaudiodata(ar);
+plot(1:length(data), data)
 %%  local testing 
 t = 1:100;
 data = sin(t*200/pi);
-% plot(data)
 %% decimation by a factor of 8 ?
-close all
 clc
-clear
-%   sample data for testing.
-t = 1:500;
-% data = sin(t*200/pi);
-data = square(t);
+close all
 fs = 8e3;
-
+t_plot = 1:1000;
 L = 8;
 N = length(data);                   %length of the data.
 x_down = 1:floor((length(data)/8)); % make array 1/8th of total 0-X; round down.
@@ -37,12 +33,12 @@ z = interp_input(y);
 
 figure;
 subplot(2, 2, 1);
-plot(1:length(data), data,'r');
+plot(t_plot, data(1:length(t_plot)),'r');
 ylabel("data IN");
 title("Data In");
 
 subplot(2, 2, 2);
-plot(1:length(data),  y, 'b');
+plot(t_plot,  y(1:length(t_plot)), 'b');
 ylabel("y IN");
 title("Decimated Output");
 
@@ -51,36 +47,40 @@ plot(1:length(z), z, 'g');
 ylabel("Z OUT");
 title("Interpolated Output");
 
+n = length(data);
+dt_fft = (abs(fft(data)).^2)*10;
+z_fft = (abs(fft(z)).^2)*10;
+
+f_plot = (0:n-1)*(8e3)/n;
+
 subplot(2, 2, 4);
-plot(1:length(z), abs(fft(z)).^2, 1:length(data), abs(fft(data)).^2);
+plot(f_plot, dt_fft,f_plot, z_fft);
 ylabel("FFT OUT");
 title("Output FFT");
-legend(["output fft", "input fft"]);
+legend(["input fft", "output fft"]);
 % digital low pass filter / upsampling 
 % theta = 2*pi*f/fs; %use the normalised frequency 
 % theta_c = pi/L; %since L and M are the same.
 % LOW PASS IS AT 500 HZ
 % arr_interp = lowpass(arr_up, 500/8e3);
-clc
-function data = decimate_input(arr)
-    %filter 
+
+function de = decimate_input(arr)
+%filter 
     data = zeros(1, length(arr));
-    data = lowpass(arr, 500, 1e3)
-    %decimate 
+    data = lowpass(arr, 500, 8e3, 'ImpulseResponse', 'iir');
+%decimate 
     sel = [1 (1:length(data)/8)*8];      % array to access 8th samples.
+    length(sel)
     de = data(sel);                       %get 8th samples.
 end 
 
-function data = interp_input(arr)
+function de = interp_input(arr)
 %decimate 
-    data =  zeros(1, length(arr)*8);          %full array.
-    sel =   [1 (1:length(arr)/8)*8];      % array to access 8th samples.
-    de(sel) = arr(1:length(sel));                       %get 8th samples.
+    data =  zeros(1, (length(arr)-1)*8); %full array 8* length
+    sel =   [1 (1:length(arr)-1)*8]; % array to access 8th samples.
+    data(sel) = arr(1:length(sel));
+    length(sel)
+    length(data)
 %filter 
-    data = lowpass(de, 500, 8e3)*8;
+    de = lowpass(data, 500, 8e3, 'ImpulseResponse', 'iir')*8;
 end 
-
-y=fft(originalData);
-n=length(originalData);
-fo = (0:n-1)*Fs_old/n;
-ogPower = abs(y).^2/n;
