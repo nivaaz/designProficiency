56820636ecfc3f5c36676186c1ded3dd06c69212
Nivaaz <nksehmbhi@gmail.com>
2019-10-16 12:31:29 +1100

    added dynamic SNR factor

--- dsp_task_4_function.m
+++ dsp_task_4_function.m
@@ -1,9 +1,8 @@
-
 function speaking = isSpeaking(data_in, fs)
     T = 1/fs;
 %     in_fft = abs(fft(data_in)).^2;             %FFT 
 %     f_fft = linspace(1, fs/2, length(in_fft)); %PLOTTING CODE FOR FFT 
-
+m_fac = 1;
     data_high = bandpass(data_in,[250,1000],fs,'Steepness',0.99);       %filtering data.
     in_fft_h = abs(fft(data_high)).^2;                                  %FFT data.
 
@@ -13,12 +12,20 @@ function speaking = isSpeaking(data_in, fs)
     
     speaking = zeros(1, N);
     step = floor((30e-3)/T)    %20 ms
-    a = sum(f_en(1:step))    %noise sum of the energy
-    b = mean(f_en)           %mean of the entire signal.
-    m = max(f_en)
+    a = sum(f_en(1:step));    %noise sum of the energy
+    b = mean(f_en);           %mean of the entire signal.
+    m = max(f_en);
+    a/b
+    if (a/b < 2)
+        m_fac = 5;
+    elseif (a/b < 20)
+        m_fac = 1.5;
+    elseif (a/b < 50)
+        m_fac = 1.5;        
+    end 
     for i = step:(N-2*step)
     %    speaking(i) =  1.5*sum(f_en_h(1:step)) < sum(f_en_h(i:(i+step))); % noise > power.
-        speaking(i) =  1.5*a< sum(f_en(i:(i+1.2*step))); % noise > power.
+        speaking(i) =  a+1/m_fac< sum(f_en(i:(i+1.2*step))); % noise > power.
     end
 %     p = audioplayer(data_in, fs);
 %     play(p, fs);
