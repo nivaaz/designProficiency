e5b5766259d34d2815745f5bcc24cb0f051bf9ca
Nivaaz <nksehmbhi@gmail.com>
2019-10-18 14:42:57 +1100

    updated solution
    Last mark was 
    25 SNR: 86 
    10 SNR: 50 
    5 SNR: 50

--- dsp_task_4_function.m
+++ dsp_task_4_function.m
@@ -1,49 +1,3 @@
-%% OPEN FILE.
-% [data_in, fs] = audioread('C:\Users\nkseh\OneDrive\Documents\Uni-Vazzy\dp\DSP\Task 4test_files\1.wav');
-clc
-close all
-[data_in1, fs] = audioread('C:\Users\nkseh\OneDrive\Documents\Uni-Vazzy\dp\DSP\5.wav');
-[data_in2, fs] = audioread('C:\Users\nkseh\OneDrive\Documents\Uni-Vazzy\dp\DSP\15.wav');
-[data_in3, fs] = audioread('C:\Users\nkseh\OneDrive\Documents\Uni-Vazzy\dp\DSP\25.wav');
-y1 = isSpeaking(data_in1, fs);
-y2 = isSpeaking(data_in2, fs);
-y3 = isSpeaking(data_in3, fs);
-
-%  NEW ALGO
-n_plot = 3;                 %how many plots.
-n=0;                        %plotting number.
-
-in_fft = abs(fft(data_in1)).^2;
-% data_high = bandpass(data_in1,[200,2000],fs,'Steepness',0.99);       %filtering data.
-dt_fft = abs(fft(data_high));
-f_fft = linspace(1, fs/2, length(in_fft));
-%
-figure;
-subplot(3, 1, 1);
-hold on;
-stem(y1);
-plot(1:length(data_in1), data_in1);
-legend(["Input", "isSpeaking"]);
-
-subplot(3, 1, 2);
-hold on;
-stem(y1);
-plot(1:length(data_in2), data_in2);
-legend(["Input", "isSpeaking"]);
-
-subplot(3, 1, 3);
-hold on;
-stem(y1);
-plot(1:length(data_in3), data_in3);
-legend(["Input", "isSpeaking"]);
-%plot of first really big.
-
-figure;
-hold on;
-stem(y1);
-plot(1:length(data_in1), data_in1);
-legend(["Input", "isSpeaking"]);
-
 function speaking = isSpeaking(data_in, fs)
     T = 1/fs;
     N = length(data_in);
@@ -51,10 +5,12 @@ function speaking = isSpeaking(data_in, fs)
     
     %   START OF COMPUTATION.
     step = floor((30e-3)/T);                             %20 ms
-    f_en = abs(data_in.^2);                              %ENERGY OF DATA IN    
-    b = mean(f_en)                                       %mean of the entire signal.
-    
+    f_en = abs(data_in.^2);                         %ENERGY OF DATA IN    
+    b = mean(f_en);                                    %mean of the entire signal.
+    noi = f_en(1:step);
     for i = step:(N-2*step)
-        speaking(i) = b/150 < mean(f_en(i:i+step));     %magic number.
+%         speaking(i) = b/180 < mean(f_en(i:i+step));     % for 25 SNR
+        speaking(i) = 1.427 < mean(f_en(i-step/2:i+step))/mean(noi);     % for 25 SNR
+
     end
 end
